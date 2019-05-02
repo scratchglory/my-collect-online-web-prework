c493c33331977067d17850062e1f97638ae52041
Adella <adellakenn@gmail.com>
2019-05-02 18:28:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
+require 'pry'
 
+def my_collect(array)
+  i = 0 
+  
+  while i < array.length 
+  yield (array[i])
+  i += 1 
+end
+end
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+
+# def my_collect(array)
+  
+#   array.collect do |name|
+#     name.split(" ").first
+#   end
+  
+# end
 

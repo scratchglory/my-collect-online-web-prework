201a5869ec2e238268de564fb7143ecc85277139
Adella <adellakenn@gmail.com>
2019-06-06 00:24:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
+def my_collect(array)
+  i = 0 
+  
+  while i < array.length 
+  yield (array[i])
+  i += 1 
+end
+end
+
+my_collect(array.collect) do |name|
+  name.split(" ").first
+end
+
+
+# def my_collect(array)
+  
+#   array.collect do |name|
+#     name.split(" ").first
+#   end
+  
+# end
 
 
 def my_collect(array)
